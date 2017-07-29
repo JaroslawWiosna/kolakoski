1d4c8667ce947713084ed1588d1ea791ba10a4bf
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-07-29 13:01:36 +0200

    Length of sequence is set by user (argv)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 std::vector<std::size_t> kolakoski(std::size_t size) {
     std::vector<std::size_t> vec;
@@ -30,14 +31,28 @@ std::vector<std::size_t> kolakoski(std::size_t size) {
     return vec;
 }
 
-int main() {
-    auto vec = kolakoski(100);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Please, set the length of sequence.\nAborting...\n";
+        throw;
+    }
+
+    std::size_t length;
+    std::istringstream ss(argv[1]);
+    if (!(ss >> length)) {
+        std::cerr << "Invalid number " << argv[1] << '\n';
+    }
+
+    auto vec = kolakoski(length);
     for (const auto& i : vec) {
         std::cout << i << ", ";
     }
     std::cout << "\b\b  " << std::endl;
 
-    std::string filename{"../kolakoskiSequences/kolakoski-100.txt"};
+    std::string filename{"../kolakoskiSequences/kolakoski-"};
+//    filename += length;
+    filename += ss.str();
+    filename += ".txt";
     std::ofstream ostrm(filename);
     if (ostrm) {
         for (const auto& i : vec) {
